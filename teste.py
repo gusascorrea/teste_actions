02e4913f54b84421618388ccafbfa1fd83d9c29e
Gustavo Correa <gustavo.araujo.correa@hotmail.com>
2024-12-10 17:09:16 -0300

    testando criar csv com pasta

--- teste.py
+++ teste.py
@@ -3,5 +3,5 @@ import pandas as pd
 # criar data frame aleatório
 df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
 # salvar data frame em arquivo CSV
-df.to_csv('dataframe.csv', index=False)
+df.to_csv('csv/dataframe.csv', index=False)
 print('Data frame salvo em csv')
