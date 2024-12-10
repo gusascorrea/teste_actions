4f0d3f9a139f5b20c3e09be21f97936af6f1c098
Gustavo Correa <gustavo.araujo.correa@hotmail.com>
2024-12-10 16:48:45 -0300

    testando salvamento de data frame em csv

---
+++ teste.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# criar data frame aleatório
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
+# salvar data frame em arquivo CSV
+df.to_csv('dataframe.csv', index=False)
