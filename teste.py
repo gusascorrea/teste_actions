fe76648413bb1f5e437358dea7fa29b668674ebc
Gustavo Correa <gustavo.araujo.correa@hotmail.com>
2025-01-10 17:58:46 -0300

    pre commit

--- teste.py
+++ teste.py
@@ -2,16 +2,11 @@ import os
 import pandas as pd
 
 # Criar diretório 'csv' se não existir
-os.makedirs('csv', exist_ok=True)
+os.makedirs("csv", exist_ok=True)
 
 # Criar data frame aleatório
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4,50,6], 'C': [7, 8, 9]})
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4,50,6], "C": [7, 8, 9]})
 # Salvar data frame em arquivo CSV
+df.to_csv("csv/dataframe.csv", index=False)
 
-
-df.to_csv('csv/dataframe.csv', index=False)
-
-
-
-
-print('Data frame salvo em csv. Pourra!')
+print("Data frame salvo em csv. Pourra!")
